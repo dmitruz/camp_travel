5affddd0f057ce526684869418a5a4933e8f3a04
dmitruz <dmitruz970@gmail.com>
2025-02-08 11:47:05 +0200

    Add stars reviews

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 type ButonProps = {
     type: 'button' | 'submit';
@@ -8,7 +9,11 @@ type ButonProps = {
 
 const Button = ({ type, title, icon, variant }: ButonProps) => {
     return (
-        <div>Button</div>
+        <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}
+        >
+            {icon && <Image src={icon} alt={title} width={24} height={24} />}
+            <label className="bold-16 whitespace-nowrap">{title}</label>
+        </button>
     )
 }
 
